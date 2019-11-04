1f6e02aa7732e6405bbba83ce6a66d873bea534b
Razvan Panea <razvan.panea@duke.edu>
2019-11-04 11:23:07 -0500

    Modify bash script to use pathnames with spaces

--- main.sh
+++ main.sh
@@ -66,7 +66,7 @@ done
 mkdir tmp
 
 # Generate the bed files
-./make_beds.py ${GTF} tmp/
+./make_beds.py "${GTF}" tmp/
 
 # Sort and merge each bed file
 for bed_file in $(find ./tmp -name "*.bed" -printf "%f\n");
@@ -74,19 +74,19 @@ do
     # Merge all files except "genes"
     if [[ ! ${bed_file} =~ ".genes." ]]
     then
-        bedtools sort -faidx ${FASTA_IDX} -i tmp/${bed_file} > tmp/sorted${bed_file}
+        bedtools sort -faidx "${FASTA_IDX}" -i tmp/${bed_file} > tmp/sorted${bed_file}
         bedtools merge -i tmp/sorted${bed_file} > tmp/merged${bed_file}
-        bedtools sort -faidx ${FASTA_IDX} -i tmp/merged${bed_file} > ${PREFIX}${bed_file}
+        bedtools sort -faidx "${FASTA_IDX}" -i tmp/merged${bed_file} > "${PREFIX}"${bed_file}
     else
-        bedtools sort -faidx ${FASTA_IDX} -i tmp/${bed_file} > ${PREFIX}${bed_file}
+        bedtools sort -faidx "${FASTA_IDX}" -i tmp/${bed_file} > "${PREFIX}"${bed_file}
     fi
 done
 
 # Generate genome.txt file for bedtools complement
-cut -f1,2 ${FASTA_IDX} > tmp/genome.txt
+cut -f1,2 "${FASTA_IDX}" > tmp/genome.txt
 
 # Generate the intergenic file
-bedtools complement -i ${PREFIX}.genes.bed -g tmp/genome.txt > ${PREFIX}.intergenic.bed
+bedtools complement -i "${PREFIX}".genes.bed -g tmp/genome.txt > "${PREFIX}".intergenic.bed
 
 # Remove temporary directory
 rm -r tmp
